fa7bf704df1ae8f721b91e57807267c24f99fcdd
jane <ch8.eva@gmail.com>
2023-03-05 20:28:59 +0300

    commit 05.03.2023

--- src/main/java/jane/AppRunner.java
+++ src/main/java/jane/AppRunner.java
@@ -1,7 +1,20 @@
 package jane;
 
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
+import java.sql.SQLException;
+
 public class AppRunner {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+    public static void main(String[] args) throws SQLException {
+
+        Configuration configuration = new Configuration();
+        configuration.configure();
+
+        try (SessionFactory sessionFactory = configuration.buildSessionFactory();
+             Session session = sessionFactory.openSession()) {
+            System.out.println("OK");
+        }
     }
 }
